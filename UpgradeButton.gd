a8d9ac717ae1bdc1b62b277813e7551c928cb3e9
Francois Mazeau <mazeau_f@epita.fr>
2019-04-28 04:29:43 +0200

    Upgrades: remove useless scripts

--- UpgradeButton.gd
+++ UpgradeButton.gd
@@ -1,18 +1,22 @@
 extends TextureButton
 
 
-var upgrades = [ 
-    preload("res://scenes/WeaponUpgrades.tscn"),
-    preload("res://scenes/SurvivalUpgrades.tscn"),
-    preload("res://scenes/AbilityUpgrades.tscn")
-]
+var upgrades = {"WeaponButton": preload("res://scenes/WeaponUpgrades.tscn"),
+                "SurvivalButton": preload("res://scenes/SurvivalUpgrades.tscn"),
+                "AbilityButton": preload("res://scenes/AbilityUpgrades.tscn")
+}
 
-func instanciate(var upgrade_type):
+
+func instanciate():
     var cur_upgrades = get_node("../../Upgrades")
     
     if (cur_upgrades != null):
         get_node("../..").remove_child(cur_upgrades)
         cur_upgrades.queue_free()
         
-    cur_upgrades = upgrades[upgrade_type].instance()
+    cur_upgrades = upgrades[self.name].instance()
     get_node("../../").add_child(cur_upgrades)
+    
+func _pressed():
+    instanciate()
+    print(self.name)
