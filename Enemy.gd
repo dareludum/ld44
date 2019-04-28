509b2e16fe8450cd35d444660d24a7e2da91d0f0
Kajetan Dutka <kdutka@nvidia.com>
2019-04-28 12:36:53 -0700

    despawn enemies when they leave the play area

--- Enemy.gd
+++ Enemy.gd
@@ -7,16 +7,24 @@ var _player
 # one of the specs in res://enemies/, use it for anything specific to a particular enemy type
 onready var spec = $Spec
 
+onready var play_area = get_node("../PlayArea")
+
 func set_target(player):
 	_player = player
 
 func _ready():
 	assert(OK == $Area2D.connect("area_entered", self, "on_area_entered"))
+	assert(OK == $Area2D.connect("area_exited", self, "on_area_exited"))
 
 func on_area_entered(area: Area2D):
 	var node = area.get_node("..")
 	if node is Blade and node.engaged:
 		# todo: take damage instead
+		self.queue_free()
+
+func on_area_exited(area: Area2D):
+	if area == play_area:
+		# we left the play area / the play area exited us
 		self.queue_free()
 
 func _process(delta):
