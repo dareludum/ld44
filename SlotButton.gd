fee49803d51b71aa57d494af260696ef23d21428
Francois Mazeau <mazeau_f@epita.fr>
2019-04-28 21:19:42 +0200

    Upgrades: add circular design placeholders

--- SlotButton.gd
+++ SlotButton.gd
@@ -1,4 +1,4 @@
-extends Button
+extends TextureButton
 
 
 var upgrades = {"WeaponButton": preload("res://scenes/WeaponUpgrades.tscn").instance(),
@@ -7,15 +7,15 @@ var upgrades = {"WeaponButton": preload("res://scenes/WeaponUpgrades.tscn").inst
 }
 
 
-func instanciate():
-    var cur_upgrades = get_node("../../Tree/Upgrades")
+func instantiate():
+    var cur_upgrades = get_node("../../../Tree/Upgrades")
     
     if (cur_upgrades != null):
-        get_node("../../Tree").remove_child(cur_upgrades)
+        get_node("../../../Tree").remove_child(cur_upgrades)
         
     cur_upgrades = upgrades[self.name]
-    get_node("../../Tree").add_child(cur_upgrades)
+    get_node("../../../Tree").add_child(cur_upgrades)
     
 func _pressed():
-    instanciate()
+    instantiate()
     print(self.name)
