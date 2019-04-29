401c622ef92d0ab2cf96adc8ad6f841d13950923
Quentin COELHO <sieramsn@gmail.com>
2019-04-29 02:16:06 +0100

    sfx: play sound when player is hurt

--- SoundEngine.gd
+++ SoundEngine.gd
@@ -3,8 +3,9 @@ extends AudioStreamPlayer
 var blade_streams: Array
 var enemy_death_streams: Array
 var game_over_streams: Array
+var player_hit_streams: Array
 
-enum SFX_TYPE {BLADE, ENEMY_DEATH, GAME_OVER}
+enum SFX_TYPE {BLADE, ENEMY_DEATH, GAME_OVER, PLAYER_HIT}
 
 func _ready():
 	_load_streams()
@@ -23,6 +24,11 @@ func _load_streams():
 	game_over_streams = [
 		preload("res://sfx/game_over.wav")
 		]
+	player_hit_streams = [
+		preload("res://sfx/player_hit1.wav"),
+		preload("res://sfx/player_hit2.wav"),
+		preload("res://sfx/player_hit3.wav")
+		]
 
 func play_sfx(type):
 	var selected_streams = null
@@ -33,6 +39,8 @@ func play_sfx(type):
 			selected_streams = enemy_death_streams
 		SFX_TYPE.GAME_OVER:
 			selected_streams = game_over_streams
+		SFX_TYPE.PLAYER_HIT:
+			selected_streams = player_hit_streams
 	if selected_streams == null:
 		return
 	self.set_stream(selected_streams[rand_range(0, selected_streams.size())])
